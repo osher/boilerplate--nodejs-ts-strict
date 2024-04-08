7c169b72872b0547fe92659e60f0541b6befb698
Osher <osher.filter@gmail.com>
2024-04-08 13:40:17 +0300

    chore: refine

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,5 @@
+import 'dotenv/config';
+
 export const Config = {
   port: Number.parseInt(process.env.PORT ?? '8080'),
 };
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,5 @@
 #!/usr/bin/env node
-
-/**
- * This is a sample HTTP server.
- * Replace this with your implementation.
- */
+// eslint-disable-next-line simple-import-sort/imports
 
 import { createServer, IncomingMessage, ServerResponse } from 'node:http';
 import Path from 'node:path';
@@ -11,21 +7,17 @@ import { fileURLToPath } from 'node:url';
 
 import { Config } from './config.js';
 
-import 'dotenv/config';
-
 const { resolve } = Path;
 const nodePath = resolve(process.argv[1]);
 const modulePath = resolve(fileURLToPath(import.meta.url));
 const isCLI = nodePath === modulePath;
 
 export default function main(port: number = Config.port) {
-  const requestListener = (request: IncomingMessage, response: ServerResponse) => {
+  const server = createServer((request: IncomingMessage, response: ServerResponse) => {
     response.setHeader('content-type', 'text/plain;charset=utf8');
     response.writeHead(200, 'OK');
     response.end(':)');
-  };
-
-  const server = createServer(requestListener);
+  });
 
   if (isCLI) {
     server.listen(port);
